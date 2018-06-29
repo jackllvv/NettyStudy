905ed5465c8edceecb5a97854fb72aabf938eb54
zhousong <zhousong@jd.com>
2018-06-29 15:14:58 +0800

    netty简单使用 粘包 粘包的解决

---
+++ src/main/java/com/zs/test5/TimeClient.java
@@ -0,0 +1,57 @@
+package com.zs.test5;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ *
+ * 粘包问题
+ * @author : zhousong
+ * Create in 2018/6/28
+ */
+public class TimeClient {
+
+    public void connect(int port, String host) throws Exception{
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+
+            //发起异步链接操作
+            ChannelFuture f = b.connect(host, port).sync();
+
+            //等待客户端链路关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            //优雅退出,释放Nio线程组
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+            }
+        }
+
+        new TimeClient().connect(port, "127.0.0.1");
+    }
+}
